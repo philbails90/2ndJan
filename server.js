57a50a00794ff2a1cee4f176a920a0645323ce3c
Phil Bailey <phil@Phils-MacBook.local>
2022-01-02 19:39:47 +0000

    passing objects worked

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+
+    const articles = [{
+        title: 'test article',
+        date: new Date(),
+        description: 'test description'
+    }]
+    res.render('index', {articles : articles})
 })
 
 app.listen(5001)
