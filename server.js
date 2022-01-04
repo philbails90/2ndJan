70d03239f8a91d1bfb5788b3ed10951f97f223e0
Phil Bailey <phil@Phils-MacBook.local>
2022-01-04 00:14:56 +0000

    starting to get confusing...

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog')
+
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
 
@@ -25,5 +28,7 @@ app.get('/', (req, res) => {
     }]
     res.render('articles/index', {articles : articles})
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5001)
